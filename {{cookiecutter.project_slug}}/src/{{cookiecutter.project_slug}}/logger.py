34073e1110ff66da24d53cfd0f91e0ecb07743db
Chris Taake <157610993+ctaake@users.noreply.github.com>
2025-07-05 11:54:08 +0200

    feat: added a default get_logger

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+def get_logger(name: str = None) -> logging.Logger:
+    """
+    Creates and confiurates a logger with own default settings
+    """
+    logger = logging.getLogger(name)
+    if not logger.hasHandlers():  # Avoids multiple handlers
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)  # Global log level
+    return logger
