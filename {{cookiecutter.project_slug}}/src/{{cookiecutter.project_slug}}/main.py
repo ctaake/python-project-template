bee4dab488439f7e7bd0701a29ba6c9b1740946a
Chris Taake <157610993+ctaake@users.noreply.github.com>
2025-07-05 12:02:11 +0200

    fix: changed default logger message to English

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/main.py
@@ -2,7 +2,7 @@ def main() -> None:
     """Default main function for utilizing the {{ cookiecutter.project_name }}."""
     from {{cookiecutter.project_slug}}.logger import getLogger
     logger = getLogger(__name__)
-    logger.info("Projekt {{ cookiecutter.project_name }} läuft.")
+    logger.info("Running project: '{{ cookiecutter.project_name }}'")
 
 
 if __name__ == "__main__":
