3bd4807591eed4f276f321726b23775963a8d960
Chris Taake <157610993+ctaake@users.noreply.github.com>
2025-07-05 11:55:17 +0200

    feat: default import of the custom logger in the main

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/main.py
@@ -1,8 +1,6 @@
-from logging import getLogger
-
-
 def main() -> None:
     """Default main function for utilizing the {{ cookiecutter.project_name }}."""
+    from {{cookiecutter.project_slug}}.logger import get_logger
     logger = getLogger(__name__)
     logger.info("Projekt {{ cookiecutter.project_name }} läuft.")
 
