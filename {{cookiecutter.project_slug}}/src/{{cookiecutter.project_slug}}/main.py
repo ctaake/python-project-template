b2e81adefbea185bcb532964438c07bad06bdccf
Chris Taake <157610993+ctaake@users.noreply.github.com>
2025-07-05 14:21:00 +0200

    fix: used new logger definition

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/main.py
@@ -1,7 +1,7 @@
 def main() -> None:
     """Default main function for utilizing the {{ cookiecutter.project_name }}."""
-    from {{cookiecutter.project_slug}}.logger import getLogger
-    logger = getLogger(__name__)
+    from {{cookiecutter.project_slug}}.logger import get_logger
+    logger = get_logger(__name__)
     logger.info("Running project: '{{ cookiecutter.project_name }}'")
 
 
